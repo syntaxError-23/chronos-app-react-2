3a0b2237c72711ba92286d6aef341242e8b29083
Josef K <ahmed.abshir91@gmail.com>
2024-08-28 21:10:07 +0100

    fixes .map issue in schedule

--- src/components/Schedule/index.jsx
+++ src/components/Schedule/index.jsx
@@ -33,7 +33,7 @@ function Schedule() {
     const [inputValue, setInputValue] = useState('');
     const [selectStartValue, setSelectStartValue] = useState('');
     const [selectEndValue, setSelectEndValue] = useState('');
-    const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem('tasksArr')));
+    const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem('tasksArr')) || []);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [taskBg, setTaskBg] = useState('');
     const [modalMsg, setModalMsg] = useState(0);
@@ -257,7 +257,7 @@ function Schedule() {
 
                     <div className="grid-of-tasks">
 
-                        {tasks.map((task, index) => {
+                        {/* {tasks.map((task, index) => {
 
                             let totalMins = 0; //Variable that stores total Minsfor each task
 
@@ -288,7 +288,46 @@ function Schedule() {
                                     </div>
                                 </div>
                             )
-                        })}
+                        })} */}
+
+
+                        {tasks && tasks.length > 0 ? (
+
+                            tasks.map((task, index) => {
+                                
+                                let totalMins = 0; //Variable that stores total Mins for each task
+
+                                //calculates differences in start and end hours/mins
+                                const taskHourDiff = parseInt(task.endHour) - parseInt(task.startHour);
+                                const taskMinDiff = parseInt(task.endMin) - parseInt(task.startMin);
+    
+                                //calculates total Mins
+                                totalMins = (taskHourDiff * 60) + taskMinDiff;
+                                console.log(`TOTAL MINS: ${totalMins}`);
+    
+                                // Calculate the duration of the task in minutes
+                                const taskDuration = (parseInt(task.endHour) * 60 + parseInt(task.endMin)) - (parseInt(task.startHour) * 60 + parseInt(task.startMin));
+
+                                return (
+                                
+                                <div className="task" key={index} data-listid={index} 
+                                style={{gridRow: `${((parseInt(task.startHour) * 6 + parseInt(task.startMin)/10) + 1)}/span ${totalMins/10}`,
+                                zIndex: 2, backgroundColor: task.colour}}> 
+                                    <div className="task-content-container">
+                                        <div className="task-buttons">
+                                            <p className="edit-btn">Edit</p>
+                                            <span className="close-btn" onClick={handleDelete}>x</span>
+                                        </div>
+                                        <div className="task-content">
+                                            <p className="task-desc">{task.desc}</p> 
+                                            <p className="task-time">{task.start} - {task.end}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            )})
+                        ) : (
+                            <p id="no-tasks-msg">No tasks available</p>
+                        )}
                     </div>
                 </div>
             </div>
